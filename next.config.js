c738895f953ec53aff0d9e7174b4bab1805c848b
MahmoudKamal01 <53492856+MahmoudKamal01@users.noreply.github.com>
2022-08-13 03:46:25 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-    env: {
-        "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "YOUR_MONGODB_URL",
-        "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
-        "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
-        "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
-        "CLOUD_UPDATE_PRESET": "YOUR_CLOUD_UPDATE_PRESET",
-        "CLOUD_NAME": "YOUR_CLOUD_NAME",
-        "CLOUD_API": "YOUR_CLOUD_API"
-    }
-}
+  env: {
+    "BASE_URL": "http://localhost:3000",
+    "MONGODB_URL":
+      "mongodb+srv://drogo:1234@cluster0.g7ulmmn.mongodb.net/?retryWrites=true&w=majority",
+    "ACCESS_TOKEN_SECRET": "0q%nmK@ZLyYBitw5myizzl@B$!dXPZ9n@X%QM^&Hnumh^YcBm1",
+    "REFRESH_TOKEN_SECRET": "66nd!6C6gu*nE7BgR7HqY8@XqPv@xj$&IAdE9A28r^zVv0cVmG",
+    "PAYPAL_CLIENT_ID":
+      "AYTRW2CZtuvepq1OV73qhysgP60zG52PpL-b0GEVnKTtbJEvuwbnrbrft9lXoJHLZE-9uYRlaAk0g-wQ",
+    "CLOUD_UPDATE_PRESET": "nextjs_store",
+    "CLOUD_NAME": "depaw8t53",
+    "CLOUD_API": "https://api.cloudinary.com/v1_1/depaw8t53/image/upload",
+  },
+};
